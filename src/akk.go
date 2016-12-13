72c225ba542f7d3184026ab0b367dfdcb8df782e
fkuril <fkuril@mail.ru>
2016-12-13 17:00:16 +0300

    first trampoline version.git

--- src/akk.go
+++ src/akk.go
@@ -39,29 +39,59 @@ func find(m,n int64) int64 {
 }
 
 func Akk(m,n int64) int64 {
-  //stack := [][]int{}
+  stack := [][]int64{}
   res := int64(0);
   //result := int64(0);
   switch {
     case m==int64(0):
       res = res + n+int64(1)
+      fmt.Printf("case2: %v\n",stack)
       memo = add(m,n,res)
     case m>0 && n==0:
       res = find(m,n)
       if res == int64(-1) {
+        stack = append(stack, []int64{m,1})
         res = Akk(m-int64(1),int64(1))
+        fmt.Printf("case2: %v\n",stack)
         memo = add(m,n,res)
       }
     case m>0 && n>0:
       res = find(m,n)
       if res == int64(-1) {
-
-        res = Akk(m-int64(1),Akk(m,n - int64(1)))
+        stack = append(stack, []int64{m,n})
+        mid := Akk(m,n - int64(1))
+        stack = append(stack, []int64{m,mid})
+        res = Akk(m-int64(1),mid)
         memo = add(m,n,res)
       }
   }
   return res
 }
+
+type akkfun func(m, n, acc int64) (akkfun, int64, int64, int64)
+
+func akk (m, n, acc int64) (akkfun, int64, int64, int64) {
+  switch {
+    case m==0:
+      return nil, 0, 0, acc+n+1
+    case m>0 && n==0:
+      return akk, m-1, 1, acc
+    default:
+      internalRes := AkkTramp(m,n-int64(1))
+      return akk, m-1, internalRes, acc
+  }
+}
+
+func AkkTramp(m, n int64) int64 {
+  fun := akkfun(akk)
+  acc := int64(0)
+
+  for fun !=nil {
+    fun, m, n, acc = fun(m, n, acc)
+  }
+  return acc
+}
+
 
 main () {
   fmt.Println(Akk(2,2))
