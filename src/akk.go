8410fa685a5c73fad11445fad6fd0bf9b1cdf9ec
fkuril <fkuril@mail.ru>
2016-12-15 17:18:45 +0300

    remove Akk

--- src/akk.go
+++ src/akk.go
@@ -41,38 +41,6 @@ func find(m,n int64) int64 {
   return res
 }
 
-// Recur with memo?
-
-func Akk(m,n int64) int64 {
-  stack := [][]int64{}
-  res := int64(0);
-  //result := int64(0);
-  switch {
-    case m==int64(0):
-      res = res + n+int64(1)
-      fmt.Printf("case2: %v\n",stack)
-      memo = add(m,n,res)
-    case m>0 && n==0:
-      res = find(m,n)
-      if res == int64(-1) {
-        stack = append(stack, []int64{m,1})
-        res = Akk(m-int64(1),int64(1))
-        fmt.Printf("case2: %v\n",stack)
-        memo = add(m,n,res)
-      }
-    case m>0 && n>0:
-      res = find(m,n)
-      if res == int64(-1) {
-        stack = append(stack, []int64{m,n})
-        mid := Akk(m,n - int64(1))
-        stack = append(stack, []int64{m,mid})
-        res = Akk(m-int64(1),mid)
-        memo = add(m,n,res)
-      }
-  }
-  return res
-}
-
 //Trampoline version
 
 type akkfun func(m, n, acc int64) (akkfun, int64, int64, int64)
